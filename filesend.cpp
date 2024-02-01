5b29875c6d45853bc4e88ed3af8a061b0bbfabba
arash039 <shojaa.arash@gmail.com>
2024-02-01 01:13:26 +0100

    html loading

--- filesend.cpp
+++ filesend.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -5,51 +6,74 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <poll.h>
+#include <string.h>
 
-// Function to read an entire file into a string
-std::string readFile(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary);
-    std::ostringstream ss;
-    ss << file.rdbuf();
-    return ss.str();
-}
+#define MAX_CLIENTS 100
 
 int main() {
-    // Create a socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in server_addr;
+    pollfd fds[MAX_CLIENTS];
+    int nfds = 1, new_fd;
 
-    // Bind the socket to a specific IP and port
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // Listen for incoming connections
-    listen(server_fd, 3);
+    bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    listen(listen_fd, 10);
 
-    while (true) {
-        // Accept a new connection
-        int new_socket = accept(server_fd, NULL, NULL);
+    fds[0].fd = listen_fd;
+    fds[0].events = POLLIN;
 
-        // Read the file data
-        std::string fileData = readFile("index.html");
+    while (1) {
+        int n = poll(fds, nfds, -1);
+        if (n == -1) {
+            perror("poll");
+            return 1;
+        }
 
-        // Create the HTTP response
-        std::ostringstream response;
-        response << "HTTP/1.1 200 OK\r\n"
-                 << "Content-Type: application/octet-stream\r\n"
-                 << "Content-Length: " << fileData.size() << "\r\n"
-                 << "Connection: close\r\n"
-                 << "\r\n"
-                 << fileData;
+        for (int i = 0; i < nfds; i++) {
+            if (fds[i].revents == 0)
+                continue;
 
-        // Send the response
-        std::string responseStr = response.str();
-        send(new_socket, responseStr.c_str(), responseStr.size(), 0);
+            if (fds[i].fd == listen_fd) {
+                new_fd = accept(listen_fd, NULL, NULL);
+                fds[nfds].fd = new_fd;
+                fds[nfds].events = POLLIN;
+                nfds++;
+            } else {
+                char buffer[1024];
+                int len = read(fds[i].fd, buffer, sizeof(buffer) - 1);
+                if (len > 0) {
+                    buffer[len] = '\0';
+                    std::cout << "Received: " << buffer << std::endl;
 
-        // Close the connection
-        close(new_socket);
+                    // Open the HTML file
+                    std::ifstream html_data("index.html");
+
+                    if (!html_data) {
+                        std::cerr << "Could not open file\n";
+                        return 1;
+                    }
+
+                    // Read the file into memory
+                    std::stringstream buffer;
+                    buffer << html_data.rdbuf();
+                    std::string response_data = buffer.str();
+
+                    // Prepare the HTTP response
+                    std::string httpResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + std::to_string(response_data.size()) + "\r\n\r\n" + response_data;
+                    write(fds[i].fd, httpResponse.c_str(), httpResponse.size());
+                } else {
+                    close(fds[i].fd);
+                    fds[i] = fds[nfds - 1];
+                    nfds--;
+                }
+            }
+        }
     }
 
     return 0;
